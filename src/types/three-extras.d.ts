d3e0a7b91954ea89db166169ba753e0e1ad8fdf4
erikso55 <eriksjn03@gmail.com>
2025-04-28 16:18:14 +0200

    del

--- src/types/three-extras.d.ts
+++
@@ -1,8 +0,0 @@
- /* Allow importing Three geometry helpers that lack @types */
-declare module 'three/examples/jsm/geometries/CapsuleGeometry.js' {
-    import { BufferGeometry } from 'three';
-    export class CapsuleGeometry extends BufferGeometry {
-      constructor(radius?:number, length?:number, capSegments?:number, radialSegments?:number);
-    }
-  }
-  
